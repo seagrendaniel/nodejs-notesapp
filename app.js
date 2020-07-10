bd3d51b715d4cd06376d08ea2423d268c7d12e2d
Dan Seagren <seagrendaniel562@gmail.com>
2020-07-09 21:12:12 -0700

    add yargs npm module to parse command line data for notes

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
-const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const msg = getNotes();
+// customize yargs version
+yargs.version('1.1.0');
 
-console.log(msg);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ', argv.title)
+    console.log('Body: ', argv.body)
+  }
+})
 
-console.log(validator.isURL('https://mead.io'));
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function () {
+    console.log('Listing all notes!')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading a note')
+  }
+})
+
+yargs.parse()
